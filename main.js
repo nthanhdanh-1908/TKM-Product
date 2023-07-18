b06e672bc1ee1a37a38bfe683311aab1ee6a9443
Nguyễn Thành Danh <138271458+nthanhdanh-1908@users.noreply.github.com>
2023-07-18 19:53:53 +0700

    Add new project food

---
+++ main.js
@@ -0,0 +1,49 @@
+// OPEN/CLOSE MENU MOBILE
+document
+  .getElementById("iconShowMenuMobile")
+  .addEventListener("click", function () {
+    document.getElementById("menuMobile").style.display = "block";
+  });
+
+document
+  .getElementById("iconCloseMenuMobile")
+  .addEventListener("click", function () {
+    document.getElementById("menuMobile").style.display = "none";
+  });
+
+document
+  .getElementById("keepMenuMobile")
+  .addEventListener("click", function () {
+    document.getElementById("menuMobile").style.display = "none";
+  });
+// END OPEN/CLOSE MENU MOBILE
+
+// START OPEN/CLOSE FORM
+document
+  .getElementById("openHeaderForm")
+  .addEventListener("click", function () {
+    document.getElementById("headerForm").style.display = "flex";
+  });
+
+document
+  .getElementById("openHeaderFormMedia")
+  .addEventListener("click", function () {
+    document.getElementById("headerForm").style.display = "flex";
+  });
+
+document
+  .getElementById("openHeaderFormEvent")
+  .addEventListener("click", function () {
+    document.getElementById("headerForm").style.display = "flex";
+  });
+
+document
+  .getElementById("closeHeaderForm")
+  .addEventListener("click", function () {
+    document.getElementById("headerForm").style.display = "none";
+  });
+// BEGIN OPEN/SLOSE FORM
+
+// END HEADER
+
+// CONTENT
